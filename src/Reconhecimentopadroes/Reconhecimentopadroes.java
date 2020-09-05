8139214bd36c06ce9e2ba80aa3ed8dc99ed57a20
eduardoluisreis <eduardoluisreis@gmail.com>
2020-09-05 15:04:03 -0300

    Programa Centro de Gravidade

--- src/Reconhecimentopadroes/Reconhecimentopadroes.java
+++ src/Reconhecimentopadroes/Reconhecimentopadroes.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 
 public class Reconhecimentopadroes {
 	public static void main(String[] args){
