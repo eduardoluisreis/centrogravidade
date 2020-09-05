213c766f34c0be07d47d13baa02278043a1260ec
eduardoluisreis <eduardoluisreis@gmail.com>
2020-09-05 19:30:15 -0300

    Retirada linhas de comentarios.

--- src/Reconhecimentopadroes/Reconhecimentopadroes.java
+++ src/Reconhecimentopadroes/Reconhecimentopadroes.java
@@ -7,36 +7,36 @@ import java.io.IOException;
 
 public class Reconhecimentopadroes {
 	public static void main(String[] args){
-		int linha; //Armazenha a linha informada no txt
-		int coluna; //Armazenha a coluna informada no txt
-		int linhaMarcada = 0; //Armazena a linha que compõe o centro de gravidade
-		int colunaMarcada = 0; //Armazena a coluna que compõe o centro de gravidade
-		int indice = 2; //Indice do vetor de doubles, descontados os valores informados para a linha e a coluna
-		double[] valoresDouble; //Armazena os valores convertidos de String para double
-		double[] somatorioLinha; //Armazena as somas de cada linha
-		double[] somatorioColuna; //Armazena as somas de cada coluna
-		double linhaMaior; //Valor arbitrário para a linha que compõe o centro de gravidade
-		double colunaMaior; //Valor arbitrário para a coluna que compõe o centro de gravidade
-		double[][] dadosTabulados; //Armazena os valores em formato matricial
+		int linha; 
+		int coluna; 
+		int linhaMarcada = 0; 
+		int colunaMarcada = 0; 
+		int indice = 2;
+		double[] valoresDouble; 
+		double[] somatorioLinha; 
+		double[] somatorioColuna; 
+		double linhaMaior; 
+		double colunaMaior; 
+		double[][] dadosTabulados; 
 		String enderecoArquivo = "/home/eduardo/NetBeansProjects/Reconhecimentopadores/src/Reconhecimentopadroes/dados.txt";
-		String dados = null; //Armazena os valores de todas as linhas lidas
-		String linhaLida; //Armazena cada linha lida
-		String[] valoresString; //Armazena os valores da String dados
-		Scanner leitor = new Scanner(System.in); //Caso queira informar o caminho do arquivo pelo console
+		String dados = null; 
+		String linhaLida; 
+		String[] valoresString; 
+		Scanner leitor = new Scanner(System.in);
 		
 		//Tratamento de exceções
 		try{
-			FileReader leitorArquivo = new FileReader(enderecoArquivo); //Lê um arquivo de texto
-			BufferedReader buffer = new BufferedReader(leitorArquivo); //Lê um fluxo de caracteres armazenando-os em um buffer
+			FileReader leitorArquivo = new FileReader(enderecoArquivo); 
+			BufferedReader buffer = new BufferedReader(leitorArquivo);
 			
-			linhaLida = buffer.readLine(); //Lê uma linha
-			dados = linhaLida; //Coloca a linha lida na String dados
+			linhaLida = buffer.readLine(); 
+			dados = linhaLida; 
 			
 			while(linhaLida != null){ 
-				linhaLida = buffer.readLine(); //Lê uma linha
-				dados += linhaLida; //Contaena a primeira linha lida com as demais
+				linhaLida = buffer.readLine(); 
+				dados += linhaLida; 
 			}
-			leitorArquivo.close(); //Fecha o FileReader
+			leitorArquivo.close(); 
 		}catch(IOException ioex){
 			System.out.println("ERRO: " + ioex.getMessage());
 		}
@@ -49,26 +49,26 @@ public class Reconhecimentopadroes {
 		
 		//Separa os valores da String dados com base em espaços
 		valoresString = dados.split(" ");
-		valoresDouble = new double[valoresString.length - 1]; //Determina o tamanho do vetor valoresDouble
+		valoresDouble = new double[valoresString.length - 1]; 
 		
-		//Converte double e adiciona cada valor de do vetor de Strings valoresString em valoresDouble
+		
 		for(int i = 0; i < (valoresDouble.length); i++){
 			valoresDouble[i] = Double.valueOf(valoresString[i]).doubleValue();
 		}
 			
-		linha = (int) valoresDouble[0]; //Obtém o valor da linha informada no txt
-		coluna = (int) valoresDouble[1]; //Obtém o valor da coluna informada no txt
+		linha = (int) valoresDouble[0];
+		coluna = (int) valoresDouble[1];
 		
-		dadosTabulados = new double[linha][coluna]; //Define o tamanho da matriz dadosTabulados
-		somatorioLinha = new double[linha]; //Define o tamanho da matriz somatorioLinha
-		somatorioColuna = new double[coluna]; //Define o tamanho da matriz somatorioColuna
+		dadosTabulados = new double[linha][coluna];
+		somatorioLinha = new double[linha];
+		somatorioColuna = new double[coluna];
 		
 		System.out.println("\nDados lidos e convertidos para double");
 		for(int i = 0; i < valoresDouble.length; i++){
 			System.out.print(valoresDouble[i] + "\n");
 		}
 			
-		//Passando os dados do vetor valoresDouble para  a matriz dadosTabulados
+		
 		for(int i = 0; i < linha; i++){
 			for(int j = 0; j < coluna; j++){
 				dadosTabulados[i][j] = valoresDouble[indice++];
@@ -95,7 +95,7 @@ public class Reconhecimentopadroes {
 		
 		linhaMaior = dadosTabulados[0][0];
 		colunaMaior = dadosTabulados[0][0];
-		//Obtém a linha e a coluna de maior peso, bem como o índice da linha e da coluna
+		
 		for(int i = 0; i < somatorioLinha.length; i++){
 			if(linhaMaior < somatorioLinha[i]){
 				linhaMaior = somatorioLinha[i];
